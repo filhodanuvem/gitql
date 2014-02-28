1b6a0d16663e862b4e1c78844be8196ad09bd39a
cloudson <claudsonweb@gmail.com>
2014-02-27 22:51:42 -0300

    Add functions with map of tokens

--- lexical/tokens.go
+++ lexical/tokens.go
@@ -14,10 +14,41 @@ const T_GREATER = 11;
 const T_SMALLER = 12; 
 const T_GREATER_OR_EQUAL = 13; 
 const T_SMALLER_OR_EQUAL= 14; 
-const T_SMALLER_EQUAL = 15; 
-const T_SMALLER_NOT_EQUAL = 16; 
 const T_EQUAL = 17; 
 const T_NOT_EQUAL = 18; 
 const T_LITERAL = 19; 
 const T_NUMERIC = 20; 
 const T_EOF = 0;
+
+var tokenNameMap map[uint8]string; 
+
+func allocMapTokenNames() {
+    if (len(tokenNameMap) == 0 ) {
+        tokenNameMap = map[uint8]string{
+            T_SELECT : "T_SELECT",
+            T_FROM  : "T_FROM",
+            T_WHERE  : "T_WHERE",
+            T_ORDER : "T_ORDER" ,
+            T_BY  : "T_BY",
+            T_LIMIT  : "T_LIMIT",
+            T_DESC  : "T_DESC",
+            T_WILD_CARD  : "T_WILD_CARD",
+            T_COMMA   : "T_COMMA",
+            T_SEMICOLON : "T_SEMICOLON",
+            T_GREATER : "T_GREATER",
+            T_SMALLER : "T_SMALLER",
+            T_GREATER_OR_EQUAL : "T_GREATER_OR_EQUAL",
+            T_SMALLER_OR_EQUAL: "T_SMALLER_OR_EQUAL",
+            T_EQUAL : "T_EQUAL",
+            T_NOT_EQUAL : "T_NOT_EQUAL", 
+            T_LITERAL : "T_LITERAL" ,
+            T_NUMERIC : "T_NUMERIC", 
+            T_EOF : "T_EOF",
+        }   
+    }
+}
+
+func TokenName(token uint8) string{
+    allocMapTokenNames()
+    return tokenNameMap[token]
+}
